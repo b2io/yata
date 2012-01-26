4988330098d384d6d39cfe5bc356018b7623bfbe
Drew Miller <dmiller@base2.io>
2012-01-26 15:24:14 -0500

    Intial RoR setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yata::Application.config.secret_token = 'e9f53451313958ecf127d670b64030149d79889f9ddbd5d90a651ca1ed0f48b5a6f149f967b456d692f558a3c009f2b945992c7957ca0ec9af3de3456818e287'
