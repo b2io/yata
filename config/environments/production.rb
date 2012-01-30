32f368e4a3c9d10232f5a40ba4154c17c28eb1d0
Drew Miller <dmiller@base2.io>
2012-01-30 01:02:24 -0500

    Updated production.rb to ensure precompilation of required js/css assets. Also, precompiled assets for Heroku deployment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,8 @@ Yata::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
+  config.assets.precompile += %w( backbone.js json2.js underscore.js twitter/bootstrap.css )
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
