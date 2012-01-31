43d4922fce24d5f2ad2547e2923efdcae37b07bd
Drew Miller <dmiller@base2.io>
2012-01-30 22:37:10 -0500

    Updated to integrate Facebook login.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :twitter, "xKDrhyRODYhEHcTXyNlyGA", "baKuDMO1i1oc274s2ucVhvHoJRPnyMIh4ab8ctiH8"
   provider :google_oauth2, "56191514429.apps.googleusercontent.com", "BJ1WKwedh73JrltZpKB058Tx", { access_type: "online", approval_prompt: "", client_options: { ssl: { ca_file: "#{Rails.root}/config/ca-bundle.crt" } } }
-  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
+  provider :facebook, "182061581901664", "140e0df7e84dc1608d7d89b0dda5871d", { client_options: { ssl: { ca_file: "#{Rails.root}/config/ca-bundle.crt" } } }
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 
 end
