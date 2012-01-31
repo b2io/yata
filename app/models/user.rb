d29f111640b765b9f4df69b4b9ef6a01ceca6129
Drew Miller <dmiller@base2.io>
2012-01-31 00:26:55 -0500

    Added explicit cast in User model for omniauth to avoid seemingly random issues with Github login on Heroku.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,14 @@ class User < ActiveRecord::Base
   has_many :todos
 
   def self.from_omniauth(auth)
-    find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
+    find_by_provider_and_uid(auth["provider"].to_s, auth["uid"].to_s) || create_with_omniauth(auth)
   end
 
   def self.create_with_omniauth(auth)
     create! do |user|
-      user.provider = auth["provider"]
-      user.uid = auth["uid"]
-      user.name = auth["info"]["name"]
+      user.provider = auth["provider"].to_s
+      user.uid = auth["uid"].to_s
+      user.name = auth["info"]["name"].to_s
     end
   end
 end
