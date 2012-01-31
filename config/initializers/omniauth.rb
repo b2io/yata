cd388a31482e02c1e0d4d097d768c061da751879
Drew Miller <dmiller@base2.io>
2012-01-30 22:21:43 -0500

    Updated to get Google OAuth2 login working.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :twitter, 'xKDrhyRODYhEHcTXyNlyGA', 'baKuDMO1i1oc274s2ucVhvHoJRPnyMIh4ab8ctiH8'
-  provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+  provider :twitter, "xKDrhyRODYhEHcTXyNlyGA", "baKuDMO1i1oc274s2ucVhvHoJRPnyMIh4ab8ctiH8"
+  provider :google_oauth2, "56191514429.apps.googleusercontent.com", "BJ1WKwedh73JrltZpKB058Tx", { access_type: "online", approval_prompt: "", client_options: { ssl: { ca_file: "#{Rails.root}/config/ca-bundle.crt" } } }
   provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 
