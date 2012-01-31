31a5cd5aa251f27fecd3a3783d59261840784e32
Drew Miller <dmiller@base2.io>
2012-01-30 23:49:08 -0500

    Updated to make omniauth use environment variables for initialization.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,9 +3,9 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # If we're in the development environment, don't verify peer SSL certificates because Windows fails hard at it.
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
 
-  provider :twitter, "xKDrhyRODYhEHcTXyNlyGA", "baKuDMO1i1oc274s2ucVhvHoJRPnyMIh4ab8ctiH8"
-  provider :google_oauth2, "56191514429.apps.googleusercontent.com", "BJ1WKwedh73JrltZpKB058Tx", { access_type: "online", approval_prompt: "" }
-  provider :facebook, "182061581901664", "140e0df7e84dc1608d7d89b0dda5871d"
-  provider :github, "287061ef6fff86d4ad86", "9855cec23f0631d465423edef4376075521cd1c8"
+  provider :twitter, ENV['YATA_TWITTER_CONSUMER_KEY'], ENV['YATA_TWITTER_CONSUMER_SECRET']
+  provider :google_oauth2, ENV['YATA_GOOGLE_CLIENT_ID'], ENV['YATA_GOOGLE_CLIENT_SECRET'], { access_type: "online", approval_prompt: "" }
+  provider :facebook, ENV['YATA_FACEBOOK_APP_ID'], ENV['YATA_FACEBOOK_APP_SECRET']
+  provider :github, ENV['YATA_GITHUB_CLIENT_ID'], ENV['YATA_GITHUB_SECRET']
 
 end
