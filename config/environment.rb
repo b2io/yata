8e84a528b404f66a756305c70d87119f097fecfe
Drew Miller <dmiller@base2.io>
2012-02-01 00:00:43 -0500

    * Removed content from the _footer partial. * Updated environment.rb to pull semver from git. * Updated the _header partial to render the semver in a label. * Re-ordered the auth providers by likelyhood of usage and/or trust.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Use the latest git tag to determine the semver of the application.
+SEMVER = `git describe --always` unless defined? SEMVER
+
 # Initialize the rails application
 Yata::Application.initialize!
