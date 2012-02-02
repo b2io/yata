948dd51dd600963f1c8f3ca4ced43783cb4b8383
Drew Miller <dmiller@base2.io>
2012-02-02 10:41:09 -0500

    Updated the environment.rb SEMVER initializer to pull the latest tag from Github. Rel: #13

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Use the latest git tag to determine the semver of the application.
-SEMVER = "v0.1.1"
+# Use the latest tag on github to determine the semver of the application.
+SEMVER = `curl http://github.com/api/v2/json/repos/show/mewdriller/yata/tags`.to_s.scan(/(?<=")([v\d\.]+)(?>")/im).sort.last.last unless defined? SEMVER
+
 
 # Initialize the rails application
 Yata::Application.initialize!
