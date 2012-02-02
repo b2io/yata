f32648cd51333bf95e0bd6cbed9d2c37354ab5a9
Zack Brown <zack.t.brown@gmail.com>
2012-02-02 13:09:10 -0500

    Added email as a column on the users table. Indexed email to avoid full-table scan when we start fetching users by email. Ensured email MUST be unique.  Still need to write tests, but I'm actually not sure how to do that, as my BDD branch hasn't been merged in yet. We should have a discussion about this at some point.

--- db/migrate/20120202174940_add_email_to_user.rb
+++ db/migrate/20120202174940_add_email_to_user.rb
@@ -1,7 +1,5 @@
 class AddEmailToUser < ActiveRecord::Migration
   def change
     add_column :users, :email, :string
-    add_column :users, :avatar_url, :string
-
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120201165811) do
+ActiveRecord::Schema.define(:version => 20120202175700) do
 
   create_table "todos", :force => true do |t|
     t.string   "text"
     t.boolean  "done"
+    t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
@@ -28,7 +28,8 @@ ActiveRecord::Schema.define(:version => 20120201165811) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "email"
-    t.string   "avatar_url"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
