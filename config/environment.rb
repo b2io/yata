8989fbbe185165b89794b74fa6abaa68be3e8c1d
Drew Miller <dmiller@base2.io>
2012-02-02 22:53:16 -0500

    * Removed precompiled assets to avoid issues locally. * Updated environment.rb to run curl silently.

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Use the latest tag on github to determine the semver of the application.
-SEMVER = `curl http://github.com/api/v2/json/repos/show/mewdriller/yata/tags`.to_s.scan(/(?<=")([v\d\.]+)(?>")/im).sort.last.last unless defined? SEMVER
+SEMVER = `curl -s http://github.com/api/v2/json/repos/show/mewdriller/yata/tags`.to_s.scan(/(?<=")([v\d\.]+)(?>")/im).sort.last.last unless defined? SEMVER
 
 
 # Initialize the rails application
