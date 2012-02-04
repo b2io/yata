192f54760b0868155a14653faba42e4aebc5e3ea
Drew Miller <dmiller@base2.io>
2012-02-04 17:05:43 -0500

    Updated profile.html.erb to include a section for managing linked accounts. #12

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,7 @@ module ApplicationHelper
   end
 
   def get_avatar_url(user = current_user, options = { size: 24 })
-    if user.avatar_url.present?
-      user.avatar_url
-    else
-      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{options[:size]}"
-    end
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{options[:size]}"
   end
 end
