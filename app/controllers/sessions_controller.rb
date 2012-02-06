948ca8d5dee69a0ff23c535ab77dbb9eb7b1397e
Drew Miller <dmiller@base2.io>
2012-02-06 09:16:38 -0500

    Reworked flow in sessions_controller.rb "create" action to allow for differentiated redirection and actions for: new, link, and merge.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,49 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # Find/Create the authorization:
+
     auth = env["omniauth.auth"]
-    unless @auth = Authorization.find_from_hash(auth)
-      @auth = Authorization.create_from_hash(auth, current_user)
+
+    # Try and find the authorization
+    @auth = Authorization.find_from_hash(auth)
+
+    # If there's a current-user:
+    if current_user
+      message = {}
+
+      # If we found an authorization:
+      if @auth
+        if @auth.user.id == current_user.id
+          message = { info: "That login is already linked to this YATA account." }
+        else
+          # TODO (DM): Implement account merges.
+          message = { error: "That login is linked to another YATA account." }
+        end
+      # Otherwise, we haven't found an existing authorization:
+      else
+        # Create a new authorization linked to this user.
+        @auth = Authorization.create_from_hash(auth, current_user)
+        message = { success: "Successfully linked that login to your YATA account." }
+      end
+
+      # Send the user back to the 'profile/#/accounts' page.
+      redirect_to profile_path + "#/accounts", flash: message
+
+    # Otherwise, there's no current-user:
+    else
+      # If we weren't able to find an existing authorization: create one.
+      unless @auth
+        @auth = Authorization.create_from_hash(auth)
+      end
+
+      # Set the current user.
+      self.current_user = @auth.user
+
+      # Send the user to the 'todos' page.
+      redirect_to todo_path, flash: { success: "Signed in!" }
+
     end
-    self.current_user = @auth.user
-    redirect_to todo_path, flash: { success: "Signed in!" }
   end
 
   def destroy
