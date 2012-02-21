95e6f8e900e5aa6e6184d57519a66670319647d5
Drew Miller <dmiller@base2.io>
2012-02-21 12:43:45 -0500

    Updated semver to 0.3.0

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 require File.expand_path('../application', __FILE__)
 
 # Use the latest tag on github to determine the semver of the application.
-SEMVER = 'v0.2.0' unless defined? SEMVER
+SEMVER = 'v0.3.0' unless defined? SEMVER
 
 # Initialize the rails application
 Yata::Application.initialize!
